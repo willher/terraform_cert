7b5ae3699968850ecda84d48900b44d5b99d14df
Willis Hertweck <willis.hertweck@cdw.com>
2021-08-31 12:29:27 -0400

    finished lesson 2

---
+++ lesson two/main.tf
@@ -0,0 +1,9 @@
+provider "google" {
+  project = "project.id"
+  region  = var.region
+}
+
+provider "google1" {
+    project = "project.id2"
+    region = var.region2
+}
---
+++ lesson two/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "Google Cloud Region"
+  type        = string
+  default     = "us-east1"
+}
+variable "region2" {
+  description = "Google Cloud Region"
+  type        = string
+  default     = "us-central1"
+}
